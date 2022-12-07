f1cffc1325a1a6614d3a049723745a3be6ae0329
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-12-07 15:22:16 +0100

    script milestone 2

--- js/main.js
+++ js/main.js
@@ -78,48 +78,136 @@ const posts = [
 
 const contenitoreGenerale = document.getElementById("container");
 
+let divPost;
+let divFooter;
+let divLike;
+let divLikeButton;
+
 
 posts.forEach(post => {
     
+    divPost = document.createElement("div");
+    divFooter = document.createElement("div");
+    divLike = document.createElement("div");
+    divLikeButton = document.createElement("div");
 
-
-
-
-    contenitoreGenerale.innerHTML += 
+    divPost.classList.add("post");
+    contenitoreGenerale.append(divPost);
+    
+    divPost.innerHTML = 
     `
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${post.author.name}</div>
-                        <div class="post-meta__time">4 mesi fa</div>
-                    </div>                    
-                </div>
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
             </div>
-            <div class="post__text">${post.content}</div>
-            <div class="post__image">
-                <img src="${post.media}" alt="${post.content}">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author.name}</div>
+                <div class="post-meta__time">${post.created}</div>
+            </div>                    
         </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="${post.content}">
+        </div>
+    `
+    
+    divFooter.classList.add("post__footer");
+    divLike.classList.add("likes", "js-likes");
 
+
+    divPost.append(divFooter);
+    divFooter.append(divLike);
+
+    
+    divLikeButton.classList.add("likes__cta")
+
+
+    divLike.append(divLikeButton);
+
+    divLikeButton.innerHTML = 
+    `
+        <a class="like-button  js-like-button" href="#" data-postid="1">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+            <span class="like-button__label">Mi Piace</span>
+        </a>
     `
 
+    divLike.innerHTML +=
+        `
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+        </div>
+    
+    `
+
+    // contenitoreGenerale.innerHTML += 
+    // `
+    //     <div class="post">
+    //         <div class="post__header">
+    //             <div class="post-meta">                    
+    //                 <div class="post-meta__icon">
+    //                     <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+    //                 </div>
+    //                 <div class="post-meta__data">
+    //                     <div class="post-meta__author">${post.author.name}</div>
+    //                     <div class="post-meta__time">${post.created}</div>
+    //                 </div>                    
+    //             </div>
+    //         </div>
+    //         <div class="post__text">${post.content}</div>
+    //         <div class="post__image">
+    //             <img src="${post.media}" alt="${post.content}">
+    //         </div>
+    //         <div class="post__footer">
+    //             <div class="likes js-likes">
+    //                 <div class="likes__cta">
+    //                     <a class="like-button  js-like-button" href="#" data-postid="1">
+    //                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    //                         <span class="like-button__label">Mi Piace</span>
+    //                     </a>
+    //                 </div>
+    //                 <div class="likes__counter">
+    //                     Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+    //                 </div>
+    //             </div> 
+    //         </div>            
+    //     </div>
+
+    // `
+
+
+    
+
+});
+
+
+const bottoneMiPiace = document.querySelectorAll("div.likes__cta");
+
+
+const miPiaceInserito = document.querySelectorAll("a.like-button");
+
+const likePost = document.querySelectorAll("b.js-likes-counter");
+
+const arrayPostMiPiace = [];
+
+bottoneMiPiace.forEach((click, index) => {
+    
+
+    click.addEventListener("click", function() {
+
+        miPiaceInserito[index].classList.add("like-button--liked");
+    
+        likePost[index].innerHTML = posts[index].likes + 1;
+
+
+        arrayPostMiPiace.push(posts[index].id);
+
+        // console.log(arrayPostMiPiace);
+
+    })
+        
 
 
 });
+
